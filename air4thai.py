c4c829735096205ca0ec3a3b5b75c382bb568abf
Krittimon Promsanar <krittimonp28@gmail.com>
2024-03-11 20:26:55 +0700

    edit air4thai to read csv

--- air4thai.py
+++ air4thai.py
@@ -13,17 +13,28 @@ station_id = "44t"
 param = "PM25,PM10,O3,CO,NO2,SO2,WS,TEMP,RH,WD,BP,RAIN"
 data_type = "hr"
 start_date = "2023-12-01"
-end_date = "2024-02-20"
+end_date = "2024-03-10"
 start_time = "00"
 end_time = "23"
 
 url = f"http://air4thai.com/forweb/getHistoryData.php?stationID={station_id}&param={param}&type={data_type}&sdate={start_date}&edate={end_date}&stime={start_time}&etime={end_time}"
 response = requests.get(url)
 response_json = response.json()
-data = pd.DataFrame.from_dict(response_json["stations"][0]["data"])
+# print(pformat(response_json))
+
+import pandas as pd
+
+pd_from_dict = pd.DataFrame.from_dict(response_json["stations"][0]["data"])
+pd_from_dict.to_csv(f"air4thai_{station_id}_{start_date}_{end_date}.csv")
+
 
 # Load your dataset containing PM25, PM10, O3, CO, NO2, SO2, WS, TEMP, RH, WD
-data = pd.read_csv("air4thai_data.csv")
+data = pd.read_csv(f"air4thai_{station_id}_{start_date}_{end_date}.csv")
+
+data.rename(columns={'TEMP': 'Temperature'}, inplace=True)
+data.rename(columns={'WD': 'Wind Direction'}, inplace=True)
+data.rename(columns={'RH': 'Relative Humidity'}, inplace=True)
+data.rename(columns={'BP': 'Atmospheric Pressure'}, inplace=True)
 
 # Convert "DATETIMEDATA" to datetime format
 data["DATETIMEDATA"] = pd.to_datetime(data["DATETIMEDATA"], format="%Y-%m-%d %H:%M:%S")
